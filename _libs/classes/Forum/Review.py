c5c9b2204b7915cff4a8ab374946393496335bdb
Ahmet Gazakbayev <gazakbaev.a@phystech.edu>
2024-05-10 00:07:04 +0300

    Updated Review class

--- _libs/classes/Forum/Review.py
+++ _libs/classes/Forum/Review.py
@@ -2,6 +2,7 @@ class Review:
     def __init__(
             self,
             id : int,
+            topic_id : int,
             author : int,
             content : str,
             banned : bool,
@@ -11,6 +12,7 @@ class Review:
             issued : int
     ):
         self.id = id
+        self.topic_id = topic_id
         self.author = author
         self.content = content
         self.banned = banned
@@ -20,6 +22,8 @@ class Review:
         self.issued = issued
     def getID(self) -> int:
         return self.id
+    def getTopicID(self) -> int:
+        return self.topic_id
     def getAuthor(self) -> int:
         return self.author
     def isBanned(self) -> bool:
@@ -32,6 +36,8 @@ class Review:
         return self.last_edit
     def getIssued(self) -> int:
         return self.issued
+    def setTopicID(self, id : int) -> None:
+        self.topic_id = id
     def setBanned(self, banned : bool) -> None:
         self.banned = banned
     def setBanMeta(self, meta : dict) -> None:
