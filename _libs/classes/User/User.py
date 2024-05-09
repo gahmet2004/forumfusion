77e1c82d4b0f46780ac62bb9deccf0a4790d2a4d
Ahmet Gazakbayev <gazakbaev.a@phystech.edu>
2024-05-09 16:10:26 +0300

    Completed and updated User classes: User, Profile, Settings, Punishment

--- _libs/classes/User/User.py
+++ _libs/classes/User/User.py
@@ -1,17 +1,24 @@
 class User:
-    def __init__(self) -> None:
-        self.id = None
-        self.tag = None
-        self.email = None
-        self.password = None
-        self.group = None
-        self.last_seen = None
-        self.registration = None
-        
-    def upload(self):
-        return
-    def download(self):
-        return
+    def __init__(
+            self,
+            id : int = None,
+            tag : str = None,
+            email :str = None,
+            password :str = None,
+            group :str = None,
+            last_seen :int = None,
+            registration : int = None
+    ):
+        """
+        get instance of User object.
+        """
+        self.id = id
+        self.tag = tag
+        self.email = email
+        self.password = password
+        self.group = group
+        self.last_seen = last_seen
+        self.registration = registration
     def getId(self) -> int:
         return self.id
     def getTag(self) -> str:
@@ -24,24 +31,9 @@ class User:
         return self.registration
     def setGroup(self, group : str) -> None:
         self.group = group
-        return self.upload()
     def setLastSeen(self, last_seen : int) -> None:
         self.last_seen = last_seen
-        return self.upload()
     def setTag(self, tag : int) -> None:
         self.tag = tag
-        return self.upload()
     def setPassword(self, password : str) -> None:
         self.password = password
-        return self.password
-
-
-def getByID(id : int) -> User:
-    """
-    Return User by identificator;
-    """
-
-def getByTag(tag : str) -> User:
-    """
-    Return User by user tag;
-    """
