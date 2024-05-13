aa162a9e33b8953dca87d89df90c35d469dab15c
Ahmet Gazakbayev <gazakbaev.a@phystech.edu>
2024-05-13 18:51:01 +0300

    updated services : serializer/deserializer, default JSON settings format set

--- _libs/services/_dictSerializer.py
+++ _libs/services/_dictSerializer.py
@@ -14,3 +14,10 @@ def deserialize(s : str) -> dict:
         else:
             response[line[0]] = line[2]
     return response
+def listIdSerialize(data : list) -> str:
+    return ','.join(data)
+def listIdDeserialize(data : str) -> list:
+    data = data.split(',')
+    for node in data:
+        node = int(node)
+    return data
